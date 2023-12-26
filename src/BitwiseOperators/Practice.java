0f9cd62de7d08cb9859aeeec88f38e70495d5327
rajan-nishad <rnishad.work@gmail.com>
2023-12-26 17:46:04 +0530

    updated

---
+++ src/BitwiseOperators/Practice.java
@@ -0,0 +1,17 @@
+package BitwiseOperators;
+
+public class Practice {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        System.out.println("a & b:" +(a & b));
+        System.out.println("a | b:" +(a | b));
+        System.out.println("~a:" +(~a));
+        System.out.println("a ^ b:" +(a ^ b));
+
+        System.out.println(10<<1);
+        System.out.println(10<<2);
+        System.out.println(10>>2);
+
+    }
+}
